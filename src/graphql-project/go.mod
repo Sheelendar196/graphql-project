4bcd4018d029a4a2494bbe80e3d581c445e3e409
Sheelendar196 <Sheelendar.Rajput@shell.com>
2024-02-25 19:58:31 +0530

    change server

--- src/graphql-project/go.mod
+++ src/graphql-project/go.mod
@@ -12,10 +12,12 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/graphql-go/graphql v0.8.1 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/newrelic/go-agent/v3 v3.30.0 // indirect
 	github.com/sosodev/duration v1.2.0 // indirect
+	gitlab.com/pragmaticreviews/graphql-go v0.0.0-20200404192823-723d44cfc4aa // indirect
 	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
--- src/graphql-project/server.go
+++ src/graphql-project/server.go
@@ -8,7 +8,9 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/newrelic/go-agent/v3/newrelic"
-	"github.com/sheelendar196/go-projects/graphql-project/graph"
+
+	graph "github.com/sheelendar196/go-projects/graphql-project/graph"
+	generated "github.com/sheelendar196/go-projects/graphql-project/graph/generated"
 	logger "golang.org/x/exp/slog"
 )
 
@@ -20,31 +22,23 @@ func main() {
 	app, err := newrelic.NewApplication(
 		newrelic.ConfigAppName(appName),
 		newrelic.ConfigDistributedTracerEnabled(true),
+		newrelic.ConfigLicense("1232323432323454323456765432345676543234"),
 		newrelic.ConfigEnabled(true),
 	)
 	if err != nil {
-		logger.Error("error starting new relic err: %v")
+		logger.Error("error starting new relic err: %v", err)
 	}
 
 	server := startServer(app)
-
 	shutdownServer(server)
-	//srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	//http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	//http.Handle("/query", srv)
-
-	//log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	//log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func startServer(app *newrelic.Application) *http.Server {
 
 	mux := http.NewServeMux()
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 	mux.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	//wrappPattern, WrappHandler := newrelic.WrapHandleFunc(app, "/query", srv.ServeHTTP)
-
 	mux.Handle("/query", srv)
 
 	server := &http.Server{
@@ -54,12 +48,12 @@ func startServer(app *newrelic.Application) *http.Server {
 		IdleTimeout:  time.Second * 60,
 		Handler:      mux,
 	}
-	go func() {
-		logger.Info("connect to http://localhost:%s/ for GraphQL playground", defaultPort)
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			logger.Error("error when start server")
-		}
-	}()
+
+	logger.Info("connect to http://localhost:%s/ for GraphQL playground", defaultPort)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Error("error when start server")
+	}
+
 	return server
 }
 
